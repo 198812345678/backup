dfb4eb858a67ac21b6530f38ba69c5a5afc17fb0
will <will@willdeMacBook-Pro.local>
2017-06-20 17:55:09 +0800

    ad

--- precipitated/src/main/java/precipitated/will/StringTest.java
+++ precipitated/src/main/java/precipitated/will/StringTest.java
@@ -41,10 +41,16 @@ public class StringTest {
     }
 
     public static void main(String[] args) {
-        StringTest test = new StringTest();
-        System.out.println(test);
-        if(test.getStr() == null) {
-            System.out.println("String == null");
-        }
+        String s1 = "Programming";
+        String s2 = new String("Programming");
+        String s3 = "Program";
+        String s4 = "ming";
+        String s5 = "Program" + "ming";
+        String s6 = s3 + s4;
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s5);
+        System.out.println(s1 == s6);
+        System.out.println(s1 == s6.intern());
+        System.out.println(s2 == s2.intern());
     }
 }
